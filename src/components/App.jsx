a37094c1cecb5ef13ba3088f73036255143e14c0
AlexandraCherepania <106696097+AlexandraCherepania@users.noreply.github.com>
2023-03-19 18:00:08 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
 // import { Switch, Route } from 'react-router-dom';
 // import AppBar from './components/AppBar/AppBar';
-import SignupForm from './SignupForm/SignupForm';
-import ColorPicker from './ColorPicker/ColorPicker';
-import Counter from '../components/Counter/Counter';
-import Clock from './Clock/Clock';
+// import SignupForm from './SignupForm/SignupForm';
+// import ColorPicker from './ColorPicker/ColorPicker';
+// import Counter from '../components/Counter/Counter';
+// import Clock from './Clock/Clock';
 import PokemonView from '../views/PokemonView';
-import Friends  from './Friends/Friends';
+// import Friends  from './Friends/Friends';
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 const containerStyles = {
   maxWidth: 1170,
